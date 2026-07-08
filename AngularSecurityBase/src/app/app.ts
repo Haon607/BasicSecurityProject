d66043a7b739326dddbe067f20bd109e7aa87b9d
Hamsa Noah <noah.hamsa@twinformatics.at>
2026-07-08 10:33:36 +0200

    added tailwind and daisyui

--- AngularSecurityBase/src/app/app.ts
+++ AngularSecurityBase/src/app/app.ts
@@ -1,6 +1,5 @@
-import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HttpBackendService } from '../services/http-backend-service';
 
 @Component({
     selector: 'app-root',
@@ -8,16 +7,4 @@ import { HttpBackendService } from '../services/http-backend-service';
     templateUrl: './app.html',
     styleUrl: './app.css',
 })
-export class App implements OnInit {
-    protected text: string = '?';
-
-    private readonly http = inject(HttpBackendService);
-    private readonly cdr = inject(ChangeDetectorRef);
-
-    ngOnInit(): void {
-        this.http.getHello().subscribe((t) => {
-            this.text = t.st;
-            this.cdr.detectChanges();
-        });
-    }
-}
+export class App {}
