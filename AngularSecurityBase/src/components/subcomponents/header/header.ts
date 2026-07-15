d5e358085fac567003b5f6862c59750fdbac4f8a
Hamsa Noah <noah.hamsa@twinformatics.at>
2026-07-15 11:01:05 +0200

    header with logout

---
+++ AngularSecurityBase/src/components/subcomponents/header/header.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthenticationService } from '../../../services/authentication-service';
+import { Svg } from '../../../helpers/svg';
+import { Role } from '../../../model/authentication';
+
+@Component({
+    selector: 'app-header',
+    imports: [RouterLink],
+    templateUrl: './header.html',
+    styleUrl: './header.css',
+})
+export class Header {
+    constructor(
+        private readonly router: Router,
+        protected readonly authenticationService: AuthenticationService,
+    ) {}
+
+    protected readonly Svg = Svg;
+    protected readonly Role = Role;
+}
--- AngularSecurityBase/src/services/authentication-service.ts
+++ AngularSecurityBase/src/services/authentication-service.ts
@@ -1,9 +1,14 @@
-import { Service, signal, WritableSignal } from '@angular/core';
+import { inject, Service, signal, WritableSignal } from '@angular/core';
 import { AuthenticationData } from '../model/authentication';
+import { DialogService } from '../components/subcomponents/dialog/dialog.service';
 
 @Service()
 export class AuthenticationService {
-    authenticationData: WritableSignal<AuthenticationData | undefined> = signal(undefined);
+    public readonly authenticationData: WritableSignal<AuthenticationData | undefined> = signal(undefined);
+    private readonly dialogService: DialogService = inject(DialogService);
 
-    constructor() {}
+    logout() {
+        this.authenticationData.set(undefined);
+        this.dialogService.dialog.next({level: 'info', message: 'Erfolgreich abgemeldet!'});
+    }
 }
